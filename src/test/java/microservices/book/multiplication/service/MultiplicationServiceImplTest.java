1a2d5de3177559883684d3f25c5a6d466951b2dc
SilverNine <>
2019-04-07 15:37:24 +0900

    03

--- src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
+++ src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
@@ -2,31 +2,33 @@ package microservices.book.multiplication.service;
 
 
 import microservices.book.multiplication.domain.Multiplication;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class MultiplicationServiceTest {
-    @MockBean
+public class MultiplicationServiceImplTest {
+    private MultiplicationServiceImpl multiplicationServiceImpl;
+
+    @Mock
     private RandomGeneratorService randomGeneratorService;
 
-    @Autowired
-    private MultiplicationService multiplicationService;
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+        multiplicationServiceImpl = new MultiplicationServiceImpl(randomGeneratorService);
+    }
+
 
     @Test
     public void createRandomMultiplicationTest() {
         given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
 
-        Multiplication multiplication = multiplicationService.createRandomMultiplication();
+        Multiplication multiplication = multiplicationServiceImpl.createRandomMultiplication();
 
         assertThat(multiplication.getFactorA()).isEqualTo(50);
         assertThat(multiplication.getFactorB()).isEqualTo(30);
